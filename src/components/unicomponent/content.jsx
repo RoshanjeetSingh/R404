dff1c302c2eab0c95ffdf7e9227d56d39e5c0008
RoshanjeetSingh <roshanjeetsingh26@gmail.com>
2025-08-26 17:42:10 +0530

    correcting image url

--- src/components/unicomponent/content.jsx
+++ src/components/unicomponent/content.jsx
@@ -1,28 +1,29 @@
 import React from 'react'
 import Content1 from './content1.jsx'
-// import Content2 from './content2.jsx'
-// import Content3 from './content3.jsx'
+import culture from '../../assets/culture.png'
+import event from '../../assets/event.png'
+import environment from '../../assets/environment.png'
 const Content = () => {
   return (
     <div>
       <Content1 
         title="Our Village Culture"
         content="Lalgarh Jattan has a rich cultural heritage full of traditions, music, and vibrant festivals."
-        imageUrl="../src/assets/culture.png"
+        imageUrl={culture}
         reverse={false}
         className="responsive-image"
       />
       <Content1
        title="Spacial events"
        content="Lalgarh Jattan hosts various events throughout the year, showcasing local art, music, and cuisine."
-       imageUrl="../src/assets/event.png"
+       imageUrl={event}
        reverse={!false}
        className="responsive-image"
       />
       <Content1
        title="Environment"
        content="The village is surrounded by beautiful landscapes, with a focus on sustainable practices and environmental conservation."
-       imageUrl="../src/assets/environment.png"
+       imageUrl={environment}
        reverse={false}
        className="responsive-image"
       />
